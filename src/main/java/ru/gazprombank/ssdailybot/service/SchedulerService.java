c07001245e54e2d9cb7354b0a07baa06714e5d7c
Nikolay Panyukov <panyukovnn@gmail.com>
2023-03-25 10:54:18 +0300

    add scheduler; fix deploy to docker gradle task

---
+++ src/main/java/ru/gazprombank/ssdailybot/service/SchedulerService.java
@@ -0,0 +1,23 @@
+package ru.gazprombank.ssdailybot.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@Profile("scheduler")
+@RequiredArgsConstructor
+public class SchedulerService {
+
+    private final DayBotManager dayBotManager;
+
+    @Scheduled(cron = "${dailybot.cron}")
+    public void schedule() {
+        log.info("Выполняется запланированный запуск");
+
+        dayBotManager.processSending(true, true);
+    }
+}
